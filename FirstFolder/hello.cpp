873de7a4ba9b8a1d18a08ec0a1e53987bb2d6e6d
unknown <mouhuser104@gmail.com>
2023-08-13 19:49:12 +0200

    second try

---
+++ FirstFolder/hello.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+int main(){
+    std::cout<<"hello world!"<<std::endl;
+    string str;
+    cout<<"Enter a string: ";
+    getline(cin, str);
+    cout<<"The string you've entered is: "<<str<<endl;
+    if(str.length()>0){
+        cout<<"this string is not empty!"<<endl;
+        cout<<"permission modified"<<endl;
+
+    }
+    return 0;
+}
